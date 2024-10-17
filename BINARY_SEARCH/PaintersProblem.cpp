705e093c0a7f89b7618b3a89cd3145d1dadef403
ileen <ileensingh14@gmail.com>
2024-10-17 16:51:18 +0530

    commit

--- BINARY_SEARCH/PaintersProblem.cpp
+++ BINARY_SEARCH/PaintersProblem.cpp
@@ -24,3 +24,58 @@ Constraints:
 1 ≤ n ≤ 105
 1 ≤ k ≤ 105
 1 ≤ arr[i] ≤ 105*/
+
+
+#include<iostream>
+using namespace std;
+
+ bool isPossible(int a[] ,int n ,int k ,long long mid){
+       long long timeSum = 0;
+       int c=1;
+       for(int i=0 ;i<n ;i++){
+           if(a[i]>mid){
+               return false;
+           }
+           if(timeSum + a[i] > mid){
+               c++;
+               timeSum = a[i];
+               if(c>k){
+                   return false;
+               }
+           }
+           else{
+               timeSum +=a[i];
+           }
+       }
+       return true;
+   }
+  
+    long long minTime(int arr[], int n, int k)
+    {
+        long long start =0;
+        long long end =0;
+        for(int i = 0 ; i<n ;i++){
+            end+=arr[i];
+        }
+        long long ans = -1;
+        
+        while(start<=end){
+            long long mid = start+(end-start)/2;
+            if(isPossible(arr , n , k , mid)){
+                ans =mid;
+                end =mid -1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+       return ans; 
+    }
+
+    int main(){
+        int arr[] = {10 , 20 ,30 ,40};
+        int n=4 ;
+        int k =2 ;
+        long long ans =minTime(arr , n ,k);
+        cout<<"The minimum time to get this job done if all painters start together is "<<ans;
+    }

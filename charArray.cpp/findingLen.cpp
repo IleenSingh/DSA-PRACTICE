32e6e247dbc8324bf72826ae2a99bb0065590a86
ileen <ileensingh14@gmail.com>
2024-11-07 08:44:44 +0530

    commit

--- charArray.cpp/findingLen.cpp
+++ charArray.cpp/findingLen.cpp
@@ -11,9 +11,21 @@ int getLength(char arr[] , int size){
     return count;
 }
 
+void replaceCharacter(char originalChar , char newChar , char arr[] ,int size ){
+    for(int i =0 ;i<size ; i++){
+        if(arr[i] == originalChar){
+            arr[i]=newChar;
+        }
+    }
+}
+
 int main(){
     char arr[100];
     
     cin>>arr;
-    cout<<"The length of the array-> "<<getLength(arr , 100)<<endl;
+    // cout<<"The length of the array-> "<<getLength(arr , 100)<<endl;
+    
+    replaceCharacter('@' , ' ' , arr ,100);
+     cout<<"The New Sentence -> "<<arr;
+
 }

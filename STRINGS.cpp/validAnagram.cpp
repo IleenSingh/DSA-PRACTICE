751ccdc36374ada4b590094883c98eb0b531d66d
ileen <ileensingh14@gmail.com>
2024-11-15 16:43:38 +0530

    commit

--- STRINGS.cpp/validAnagram.cpp
+++ STRINGS.cpp/validAnagram.cpp
@@ -2,17 +2,17 @@
 #include<algorithm>
 using namespace std;
 
-bool anagram(string s , string t){
-    sort(s.begin() , s.end());
-    sort(t.begin() , t.end());
-    if(s==t){
-        return true;
-    }
-    else{
-        return false;
-    }
+// bool anagram(string s , string t){
+//     sort(s.begin() , s.end());
+//     sort(t.begin() , t.end());
+//     if(s==t){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
 
-}
+// }
 
 int main(){
     string s = "silent";

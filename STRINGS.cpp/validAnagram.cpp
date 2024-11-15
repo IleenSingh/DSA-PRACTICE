1da0004285e766c8bc58e92f9a97bdfb1e4df5d5
ileen <ileensingh14@gmail.com>
2024-11-15 17:03:37 +0530

    commit

--- STRINGS.cpp/validAnagram.cpp
+++ STRINGS.cpp/validAnagram.cpp
@@ -2,6 +2,9 @@
 #include<algorithm>
 using namespace std;
 
+
+//method 1 sorting
+
 // bool anagram(string s , string t){
 //     sort(s.begin() , s.end());
 //     sort(t.begin() , t.end());
@@ -13,6 +16,31 @@ using namespace std;
 //     }
 
 // }
+
+// method 2 Counting
+
+bool anagram(string s , string t){
+    int freqTable[256] = {0}; //ek arrat h 255 ka sab zero se initialised hai
+    //freq table bnayenge s ka
+    for(int i=0 ;i<s.size() ; i++){
+        freqTable[(int)s[i]]++;
+    }
+
+    //frq table match kr k decrement krenge
+     for(int i=0 ;i<t.size() ; i++){
+        freqTable[(int)t[i]]--;
+    }
+    
+    //check krenge saare elements zero h ki ni 
+    for(int i=0 ; i<256 ; i++){
+        if(freqTable[i] != 0){
+            return false;
+        }
+    }
+    return true;
+
+
+}
 
 int main(){
     string s = "silent";

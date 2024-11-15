03bbd82500ae3137991066b3a19ec8fc32dbf2ab
ileen <ileensingh14@gmail.com>
2024-11-15 09:18:43 +0530

    commit

---
+++ STRINGS.cpp/validAnagram.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool anagram(string s , string t){
+    sort(s.begin() , s.end());
+    sort(t.begin() , t.end());
+    if(s==t){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+int main(){
+    string s = "silent";
+    string t = "listen";
+    cout<<"s is a anagram of t -> "<<anagram(s,t);
+}

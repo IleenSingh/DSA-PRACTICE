f73ee05176fe5ee2771cb7353ff1cb79fd244bba
ileen <ileensingh14@gmail.com>
2024-11-16 11:04:37 +0530

    commit

--- STRINGS.cpp/reverseVowels.cpp
+++ STRINGS.cpp/reverseVowels.cpp
@@ -27,3 +27,40 @@ Constraints:
 1 <= s.length <= 3 * 105
 s consist of printable ASCII characters.
 */
+
+#include<iostream>
+using namespace std;
+
+bool isVowel(char ch){
+    ch=tolower(ch);//zyada condition ni likhna padega
+    return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
+}
+
+string reverseVowels(string s){
+    //do pointer intialise krenge
+    int l=0 ;
+    int h=s.size()-1;
+    while(l<h){
+        //1st case
+        if(isVowel(s[l]) && isVowel(s[h])){
+            swap(s[l] , s[h]);
+            l++;
+            h--;
+        }
+
+        //2nd case
+        else if(!isVowel(s[l])){
+            l++;
+        }
+        //3rd case
+        else{
+            h--;
+        }
+    }
+    return s;
+}
+
+int main(){
+   string s = "PrAkash";
+   cout<<"s after reversing it -> "<<reverseVowels(s);
+}

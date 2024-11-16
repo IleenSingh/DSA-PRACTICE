384d57987e26af7069b61b8c711315448cf8342d
ileen <ileensingh14@gmail.com>
2024-11-16 10:39:11 +0530

    commit

--- STRINGS.cpp/longestCommonPrefix.cpp
+++ STRINGS.cpp/longestCommonPrefix.cpp
@@ -22,3 +22,44 @@ Constraints:
 0 <= strs[i].length <= 200
 strs[i] consists of only lowercase English letters.
 */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+string longestCommonPrefix( vector<string>s ){
+    string ans;
+    int i=0;//loop chalane k liye elements pe
+    while(true){//infinite loop
+    char curr_ch = 0;
+    //sare string pe iterate krne k liye for each loop
+    for(auto str : s){
+        //out of bound wala case
+        if(i>=str.size()){
+            curr_ch = 0;
+            break;
+        }
+        //ek character se staert krne k liye comparision
+        if(curr_ch==0 ){
+            curr_ch=str[i] ;
+        }
+        else if(str[i] != curr_ch){ //equal hi ni h dusri string ka first lletter toh break krrdenge
+            curr_ch = 0;
+            break;
+        }
+    }//break k baad for loop se bhar aagye 
+    if(curr_ch == 0){ 
+        break;
+        } //while loop se bhar aane ka 
+
+    ans.push_back(curr_ch);
+    i++;
+    }
+    return ans;
+}
+
+
+int main(){
+    vector<string> s ={ "flower","flow","flight"};
+    cout<<"the longest common prefix string is -> "<<longestCommonPrefix(s)<<endl;
+}

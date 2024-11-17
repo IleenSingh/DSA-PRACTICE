3d22b6f8518b4e5b75f2199262b512f996263b34
ileen <ileensingh14@gmail.com>
2024-11-17 13:00:55 +0530

    commit

--- STRINGS.cpp/isomorphicStrings.cpp
+++ STRINGS.cpp/isomorphicStrings.cpp
@@ -49,3 +49,29 @@ s and t consist of any valid ascii character.
 
 #include<iostream>
 using namespace std;
+
+bool isomorphicStrings(string s , string t){
+      //s k characters ko mapp krenge with respect to t k characters
+    int hash[256] = {0};
+    // store krenge true values agar woh already mapped hai s[i] mai 
+    bool itTCharmapped[256] ={0};
+
+    //s or t k mapping k liye loop
+    for(int i=0 ; i<s.size() ; i++){
+        //dono char k place ko check krenge ki mapped h ki ni
+        if(hash[s[i]] ==0 && itTCharmapped[t[i]]==0){
+            hash[s[i]] = t[i] ; //mtlb t ka first character s mai mapped hogya h 
+            itTCharmapped[t[i]] = true ; //jesi hi s mai mapped hua h wesi t k uss char ko true krrdenge ki woh mapped h ek se ab dusre letter se ni hoga
+        }
+    }
+
+    //incase phle se mapped h ya mapp hua hi ni h wala case
+    for(int i=0 ; i<s.size() ;i++){
+        if(char(hash[s[i]]) != t[i]){
+            return false ;
+        }
+        
+    }
+
+    return true;
+}

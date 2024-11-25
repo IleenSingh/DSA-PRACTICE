bb2c74ac668e136cd72e8bda669b904ae4421aac
ileen <ileensingh14@gmail.com>
2024-11-25 16:55:21 +0530

    commit

--- STRINGS.cpp/longestPalindromicSubstr.cpp
+++ STRINGS.cpp/longestPalindromicSubstr.cpp
@@ -17,7 +17,7 @@ string longestPalindromicSubstr(string s){
     //ans ki empty string jisme store krenge final longest string
     string ans="";
     for(int i =0 ;i<s.size() ; i++){
-        for(int j=1 ; j<s.size() ; j++){
+        for(int j=i ; j<s.size() ; j++){
             if(isPalindrome(s,i,j)){
                 string t = s.substr(i , j-i+1);
                 ans = t.size()>ans.size()?t:ans;

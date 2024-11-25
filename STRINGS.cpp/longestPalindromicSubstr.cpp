194ec8e5c93b8e09858922127cad047fb0f96b86
ileen <ileensingh14@gmail.com>
2024-11-25 16:45:53 +0530

    commit

---
+++ STRINGS.cpp/longestPalindromicSubstr.cpp
@@ -0,0 +1,35 @@
+//brute force naive approach
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string&s ,int i , int j){
+    while(i<j){
+        if(s[i] != s[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+string longestPalindromicSubstr(string s){
+    //ans ki empty string jisme store krenge final longest string
+    string ans="";
+    for(int i =0 ;i<s.size() ; i++){
+        for(int j=1 ; j<s.size() ; j++){
+            if(isPalindrome(s,i,j)){
+                string t = s.substr(i , j-i+1);
+                ans = t.size()>ans.size()?t:ans;
+
+            }
+        }
+    }
+    return ans;
+}
+
+int main(){
+    string s ="babad";
+    string ans = longestPalindromicSubstr(s);
+    cout<<"The Longest Palindromic Substring of This String -> "<<ans<<endl;
+}

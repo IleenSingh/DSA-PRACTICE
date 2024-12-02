2ea83d9960e660021fbba386b1fea4e00c2fb800
ileen <ileensingh14@gmail.com>
2024-12-02 19:13:35 +0530

    commit

--- STRINGS.cpp/zigzagConversion.cpp
+++ STRINGS.cpp/zigzagConversion.cpp
@@ -1,3 +1,55 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
 
+string convert(string s , int numRows){
+
+    //agar ek hi row ho toh 
+    if(numRows == 1) return s ;
+
+    //zigzag krwane k liye vector bnayenge total rows ki  size ki saari rows k liye
+    vector<string>zigzag(numRows);
+
+    int i=0 ;//saare characters pe iterate krne k liye
+    int row = 0;
+    bool direction = 1 ;//first case mai dalne k liye mtlb top to bottom jo straight chlega hi uske liye
+
+    while(true){
+        if(direction){
+            while(row<numRows && i<s.size()) {
+                 //mtlb first line pura chlega 
+                 zigzag[row++].push_back(s[i++]); //row zero mai a push hoga phir b phir c               
+            }
+            row = numRows - 2; //ab zigzag pattern start krne k liye condition           
+        }
+        else{
+            //bottom to top jarhe hai isme
+            while(row>=0 && i<s.size()){
+                zigzag[row--].push_back(s[i++]);
+            }
+            //zero tkk pahuch gya ab usko wapis one pe bhej do top to bottom k liye
+            row =1;
+        }
+
+        if(i>=s.size()) break;  //loop se bhar aane ka condition 
+        direction = !direction ; //direction change krne k liye condirion iss case mai zero hoga direction
+    }
+
+    string ans ="" ; //answer store krne k liye empty string
+    //row wise puri strin ko return krwane k liye loop
+    for(int i = 0 ; i<zigzag.size() ; i++){
+        ans+=zigzag[i] ; //concatenate hojaenge saare string
+    }
+  
+   return ans;
+
+
+
+}
+
+int main(){
+    string s ="ABCDEFGHIJ";
+    int numRows= 3 ;
+    cout<<"ZIGZAG STRING - > "<<convert(s ,numRows);
+}

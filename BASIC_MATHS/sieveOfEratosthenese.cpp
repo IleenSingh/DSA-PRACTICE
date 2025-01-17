61027fce344bc2b30288ce2a719cdcf3f0969280
ileen <ileensingh14@gmail.com>
2025-01-17 18:42:36 +0530

    commit

--- BASIC_MATHS/sieveOfEratosthenese.cpp
+++ BASIC_MATHS/sieveOfEratosthenese.cpp
@@ -19,8 +19,25 @@ vector<bool>SieveOfEratosthenese(int n){
     return sieve;
 }
 
+vector<bool>optiSieveOfEratosthenese(int n){
+    //creating a array in which all are marked prime
+    vector<bool>sieve(n+1 , true);
+    sieve[0]=sieve[1]=false; //bcoz inke factor 2 se kmm rhte hai 
+
+    for(int i=2 ; i*i<=n ; i++){ //outer loop optimization
+        if(sieve[i]==true){
+            int j= i*i ;//inner loop optimization
+            while(j<=n){
+                sieve[j]=false;
+                j+=i;
+            }
+        }
+    }
+    return sieve;
+}
+
 int main(){
-    vector<bool>sieve = SieveOfEratosthenese(100);
+    vector<bool>sieve = optiSieveOfEratosthenese(100);
     for(int i =0 ; i<=100 ;i++){
         if(sieve[i]){
             cout<<i<<" ";

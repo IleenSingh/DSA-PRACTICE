99b2c5fd555999d9b24d780e3760eff9f3663300
ileen <ileensingh14@gmail.com>
2025-01-20 17:05:47 +0530

    commit

---
+++ POINTERS/charArrayWithPtrs.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch[100] = "ileenprakash";
+    char* cptr = ch;
+    cout<<"Address of ch :" <<&ch<<endl;
+    cout<<"cptr : "<<cptr<<endl;
+}

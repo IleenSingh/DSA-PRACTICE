affbb0e3fe4999a882e1b55bc746a32dbaecee89
ileen <ileensingh14@gmail.com>
2025-01-20 16:36:53 +0530

    commit

---
+++ POINTERS/ptrWithArray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //pointer with array
+    int arr[] ={10,20,30,40,50};
+    //declaration of array wrt pointer
+    int* ptr = arr;
+    cout<<"arr : "<<arr<<endl;
+    cout<<"arr= *arr+1 : "<<*arr+1<<endl;
+    cout<<"arr= *(arr+1) : "<<*(arr+1)<<endl;
+
+}

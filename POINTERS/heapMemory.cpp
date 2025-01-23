316a5066302ab8e63d74a7b0b0acf996d93af0be
ileen <ileensingh14@gmail.com>
2025-01-23 17:31:41 +0530

    commit

---
+++ POINTERS/heapMemory.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // heap memory allocation
+    int *p =new int;
+    *p =100;
+    cout<<*p<<endl;
+    //deallocation
+    delete p;
+    // cout<<*p;
+}

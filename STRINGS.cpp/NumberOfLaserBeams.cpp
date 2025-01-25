883cbe879062dda6984e4c4dcc89a677597b3632
ileen <ileensingh14@gmail.com>
2025-01-25 13:10:45 +0530

    commit

--- STRINGS.cpp/NumberOfLaserBeams.cpp
+++ STRINGS.cpp/NumberOfLaserBeams.cpp
@@ -1,7 +1,7 @@
 /*
 2125. Number of Laser Beams in a Bank
 Medium
-Topics
+Topicss
 Companies
 Hint
 Anti-theft security devices are activated inside a bank. You are given a 0-indexed binary string array bank representing the floor plan of the bank, which is an m x n 2D matrix. bank[i] represents the ith row, consisting of '0's and '1's. '0' means the cell is empty, while'1' means the cell has a security device.
@@ -47,3 +47,44 @@ n == bank[i].length
 1 <= m, n <= 500
 bank[i][j] is either '0' or '1'.
 */
+
+#include<vector>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int numberOfBeams(vector<string>& bank){
+    //string mai present h list usko convert krenge vector mai 
+    vector<int>deviceCount;
+    //iterate krenge vector pe count nikalne k liye
+    for(string& row : bank){
+        int count = 0 ;
+        //ab saare elements k ek ek chracter pr traverse krke number of 1 find krenge
+        for (char ch : row){
+            if(ch == '1'){
+                count ++;
+            }
+        }
+
+        //ab saare elements k internal count aagye hai ab push krenge sirf  greater than zero walo ko
+        if(count>0){
+            deviceCount.push_back(count);
+        }
+    }
+
+    //total number of laser beams find krenge 
+    int totalBeams =0;
+    for(int i = 1 ; i<deviceCount.size() ;i++){
+        totalBeams+= deviceCount[i-1] * deviceCount[i] ;//previous count * current count
+    }
+
+    return totalBeams ;
+
+
+}
+
+int main(){
+    vector<string> bank ={"011001","000000","010100","001000"};
+    cout<<"The Total number of laser beams -> "<<numberOfBeams(bank)<<endl;
+}

a1492ba6030f76d330dc2c0cfc00582472ae7135
ileen <ileensingh14@gmail.com>
2025-01-25 12:42:53 +0530

    commit

--- RECURSION/countingPrint.cpp
+++ RECURSION/countingPrint.cpp
@@ -14,6 +14,22 @@ void printCounting(int n){
     printCounting(n-1);
 }
 
+void printCountingReverse(int n){
+    //apn ko n se leke 1 tkk counting print krna hai 
+    //for example n=5 then output will be 54321
+    //base case ->  we know ki one tkk krwana h print toh jese hi zero pe jaega return krwa denge function ko
+    if(n==0) return;
+
+    //recursive call
+    printCountingReverse(n-1);
+    //processing
+    //ek ek bar print krwa k minus krega recursion then print
+    cout<<n<<" ";
+    
+}
+
 int main(){
     printCounting(50);
+    cout<<endl;
+    printCountingReverse(50);
 }

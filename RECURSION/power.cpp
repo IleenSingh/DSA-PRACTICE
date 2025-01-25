2cb39be4e1d0634b83499d4313760d3f0100cd39
ileen <ileensingh14@gmail.com>
2025-01-25 13:07:50 +0530

    commit

---
+++ RECURSION/power.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int pow(int n){
+    //base case -> we know that power of any number is zero then its equal to one 
+    if(n==0) return 1;
+
+    //recursive call
+    //pow(n) = 2 *pow(n-1);
+    int recursionPart = pow(n-1);
+    int Answer = 2 * recursionPart ;
+    return Answer;
+}

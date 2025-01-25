790d19fab9c625bbb6c066e69111125b98af6e88
ileen <ileensingh14@gmail.com>
2025-01-25 15:00:37 +0530

    commit

---
+++ RECURSION/SumFromOneToN.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int getSum(int n){
+    //base case -> sum find krna h 1 se n tkk ka toh 1 tkk hi chlega recursion means
+    if(n==1) return 1;
+
+    //recursion relation 
+    //sum(n) = sum(n-1) + n;
+    int Answer = getSum(n-1) + n;
+    return Answer;
+}

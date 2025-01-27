945f6e1a6af3574c20c7d6711d199f5ffd35b157
ileen <ileensingh14@gmail.com>
2025-01-27 15:44:51 +0530

    commit

---
+++ RECURSION/printAllEvenInArr.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void printAllEven (int arr[] ,int n ,int index){
+    // base case -> jab saare arrAy pe traverse krrle tbb rukhna hai 
+    if(index ==n){
+        return;
+    }
+
+    //1 case -> & operator se odd pata krrlenge 
+    if(((arr[index])&1 )== 0){
+        cout<<arr[index]<<" ";
+    }
+    //recursion part -> increment krte jaega index ko 
+    printAllEven(arr , n ,index+1);
+
+}
+
+int main(){
+    int arr[] ={1,2,3,4,5,6,7,8,9,10};
+    int n= 10;
+    int index =0 ;
+    printAllEven(arr , n ,index);
+}

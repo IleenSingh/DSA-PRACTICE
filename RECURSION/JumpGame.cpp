1f0f47b03238eb3f1afc9ffb20849af2a03f61b5
ileen <ileensingh14@gmail.com>
2025-02-08 19:05:33 +0530

    commit

---
+++ RECURSION/JumpGame.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool solve(vector<int>&nums , int i){
+    //base case-1 when the end location is reached
+    if(i == nums.size()) return true;
+    //base case-2  when its jump outside the nums ,means simply its goes out of bound
+    if(i>=nums.size()) return false;
+    //base case -3 when nums[i] =0 means we stuck at this location 
+    if(nums[i] ==0) return false;
+
+    //phle recursion k answer ko false se initialise krwaenge 
+    bool reAnswer = false;
+
+    //ab loop se ek case hmm chalaenge means one jump baaki k recursion smbhalega
+    for(int jump =1 ;jump<=nums[i] ;jump++){
+        reAnswer = reAnswer || solve(nums , i+jump);
+
+    }
+    return reAnswer;
+
+}
+
+
+bool canJump(vector<int>&nums){
+    return solve(nums,0);
+}

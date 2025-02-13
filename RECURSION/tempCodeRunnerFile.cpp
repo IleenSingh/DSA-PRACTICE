eb96e5aa90a42539f9c07e073a18b81f03e6123b
ileen <ileensingh14@gmail.com>
2025-02-13 19:37:31 +0530

    commit

--- RECURSION/tempCodeRunnerFile.cpp
+++ RECURSION/tempCodeRunnerFile.cpp
@@ -1,38 +1,3 @@
-/*
-415. Add Strings
-Easy
-Topics
-Companies
-Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
-
-You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
-
- 
-
-Example 1:
-
-Input: num1 = "11", num2 = "123"
-Output: "134"
-Example 2:
-
-Input: num1 = "456", num2 = "77"
-Output: "533"
-Example 3:
-
-Input: num1 = "0", num2 = "0"
-Output: "0"
- 
-
-Constraints:
-
-1 <= num1.length, num2.length <= 104
-num1 and num2 consist of only digits.
-num1 and num2 don't have any leading zeros except for the zero itself.
-*/
-
-#include<iostream>
-#include<string>
-using namespace std;
 
 string answer(string&num1 , int i ,string&num2 ,int j ,int carry){
     //base case ->if both indices are become zero and no carry left
@@ -62,10 +27,4 @@ string answer(string&num1 , int i ,string&num2 ,int j ,int carry){
 string addStrings(string num1 , string num2){
     return answer(num1 , num1.size()-1 ,num2 ,num2.size()-1 ,0);
     //zero for carry
-}
-
-int main(){
-    string num1 ="456";
-    string num2 ="77";
-    cout<<"The sum of the Strings ->"<<addStrings(num1 ,num2);
 }
